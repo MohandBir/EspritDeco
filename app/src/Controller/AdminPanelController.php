c4e6fd92a71181010f8f46186ba84ea8ad50fbd1
MohandBir <birmoho@gmail.com>
2026-05-04 12:58:44 +0200

    lot2-1: espace admin - midlewere panel

--- app/src/Controller/AdminPanelController.php
+++ app/src/Controller/AdminPanelController.php
@@ -5,12 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\Role\Role;
 
 final class AdminPanelController extends AbstractController
 {
     #[Route('/admin/panel', name: 'app_admin_panel')]
     public function index(): Response
     {
+        if (!$this->isGranted('ROLE_ADMIN')) {
+
+            return $this->redirectToRoute('app_login');
+        }
+
         return $this->render('admin/index.html.twig');
     }
 }
