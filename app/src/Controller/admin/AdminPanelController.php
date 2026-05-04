60239498349001990de300b98afbd88bf7a43307
MohandBir <birmoho@gmail.com>
2026-05-04 20:55:45 +0200

    lot2-2: rectification : btn-retour, color-tr

--- app/src/Controller/admin/AdminPanelController.php
+++ app/src/Controller/admin/AdminPanelController.php
@@ -1,11 +1,10 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Core\Role\Role;
 
 final class AdminPanelController extends AbstractController
 {
