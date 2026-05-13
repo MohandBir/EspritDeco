5408290dba1406d7f40d0730b61fba81f9e9f7e5
MohandBir <birmoho@gmail.com>
2026-05-13 11:50:00 +0200

    lot3-2b: recapitulatif de commande + resolving bugs with test

--- app/src/Controller/AdressController.php
+++ app/src/Controller/AdressController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Adress;
 use App\Form\AdressType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,11 +12,31 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class AdressController extends AbstractController
 {
-    #[Route('/adress', name: 'app_adress_index')]
-    public function index(Request $request): Response
+    public function __construct(private EntityManagerInterface $em) {}
+
+    #[Route('/adress', name: 'app_adress_index', methods: ['GET'])]
+    public function index(): Response
     {
         $form = $this->createForm(AdressType::class);
+
+        return $this->render('adress/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/adress/edit/{id}', name: 'app_adress_edit', methods: ['GET', 'POST'])]
+    public function edit(Adress $adress, Request $request): Response
+    {
+        $form = $this->createForm(AdressType::class, $adress);
         $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->flush();
+
+            return $this->redirectToRoute('app_order_recap', [
+                'id' => $adress->getCustomerOrder()->getId(),
+            ]);
+        }
 
         return $this->render('adress/index.html.twig', [
             'form' => $form->createView(),
--- app/src/Form/AdressType.php
+++ app/src/Form/AdressType.php
@@ -3,14 +3,14 @@
 namespace App\Form;
 
 use App\Entity\Adress;
-use App\Entity\Order;
-use Dom\Text;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Positive;
 
 class AdressType extends AbstractType
 {
@@ -19,15 +19,35 @@ class AdressType extends AbstractType
         $builder
             ->add('fullname', TextType::class, [
                 'label' => 'Nom et Prénom',
+                'constraints' => [
+                    new NotBlank(message: 'Le nom et prénom sont obligatoires.'),
+                    new Length(max: 100, maxMessage: 'Le nom ne peut pas dépasser {{ limit }} caractères.'),
+                ],
             ])
             ->add('street', TextType::class, [
-                'label' => 'Rue'
+                'label' => 'Rue',
+                'constraints' => [
+                    new NotBlank(message: 'La rue est obligatoire.'),
+                    new Length(max: 255, maxMessage: 'La rue ne peut pas dépasser {{ limit }} caractères.'),
+                ],
             ])
             ->add('zipCode', NumberType::class, [
                 'label' => 'Code postal',
+                'constraints' => [
+                    new NotBlank(message: 'Le code postal est obligatoire.'),
+                    new Positive(message: 'Code postal invalide'),
+                    new Length(
+                        min: 5, minMessage: 'Le code postal doit contenir {{ limit }} chiffre',
+                        max: 5, maxMessage: 'Le code postal doit contenir {{ limit }} chiffre',
+                    )
+                ],
             ])
             ->add('city', TextType::class, [
                 'label' => 'Ville',
+                'constraints' => [
+                    new NotBlank(message: 'La ville est obligatoire.'),
+                    new Length(max: 50, maxMessage: 'La ville ne peut pas dépasser {{ limit }} caractères.'),
+                ],
             ])
         ;
     }
